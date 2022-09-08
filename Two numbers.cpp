629398612b62f5cb1cbceb0f472843b90b2f17c0
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-09-09 04:00:23 +0600

    H. Two numbers

---
+++ Two numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    double a,b,c;
+    cin>>a>>b;
+
+
+    cout<<"floor "<<a<<" / "<<b<<" = "<<floor(a/b)<<endl;
+    cout<<"ceil "<<a<<" / "<<b<<" = "<<ceil(a/b)<<endl;
+    cout<<"round "<<a<<" / "<<b<<" = "<<round(a/b)<<endl;
+
+
+return 0;
+}
