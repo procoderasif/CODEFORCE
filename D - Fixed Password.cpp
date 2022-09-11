32eed359a1a9e122ebdf9f9d4dc9471fd0f5f418
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-09-12 01:55:03 +0600

    D - Fixed Password.cpp

---
+++ D - Fixed Password.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a=0,i,j,n;
+    int x=1999;
+
+
+
+        for(i=1; i>0; i++)
+        {
+            cin>>j;
+            if(j==x)
+            {
+                cout<<"Correct"<<endl;
+                break;
+            }
+            else
+            {
+                cout<<"Wrong"<<endl;
+            }
+        }
+
+
+    return 0;
+}
