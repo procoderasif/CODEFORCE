eac9e6fe0a044ec0fdfc556c02a7592de4225212
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-09-12 12:31:13 +0600

    F. Multiplication table.cpp

---
+++ F. Multiplication table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int N,multiple,i;
+cin>>N;
+for(i=1;i<=12;i++){
+ multiple=i*N;
+ cout<<N<<" * "<<i<<" = "<<multiple<<endl;
+}
+
+return 0;
+}
