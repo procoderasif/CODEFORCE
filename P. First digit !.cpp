90151067d8a0e6b6cd830eaaefa96e9af789ad8f
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-10-21 20:55:35 +0600

    P. First digit !

---
+++ P. First digit !.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   char a,b,c,d;
+
+   cin>>a>>b>>c>>d;
+
+   if(a%2==0)
+   {
+       cout<<"EVEN"<<endl;
+   }
+   else if(a%2!=0)
+   {
+       cout<<"ODD"<<endl;
+   }
+
+
+
+
+
+    return 0;
+}
