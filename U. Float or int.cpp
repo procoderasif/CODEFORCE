91d35b64101fb1b9c0b99901e64749bc9fa81a6c
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-11-04 02:04:59 +0600

    U. Float or int

---
+++ U. Float or int.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+double a;
+int b;
+cin>>a;
+b=a;
+if(b-a==0){
+    cout<<"int "<<b<<endl;
+}
+else{
+    cout<<"float "<<b<<" "<<a-b;
+}
+
+return 0;
+}
