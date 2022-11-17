9e1298fd3c88b85b4da389ed052cb5b992602456
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-11-17 18:35:11 +0600

    N. Numbers Histogram

---
+++ N. Numbers Histogram.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    int a,i,n,j;
+    cin>>a;
+    for(i=1;i<=a;i++){
+        cin>>n;
+        for(j=0;j<n;j++){
+            cout<<ch;
+        }
+       cout<< endl;
+    }
+
+
+    return 0;
+}
